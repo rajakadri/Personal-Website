904edb0ba83da1074986860a5936e2330d2adf47
rajakadri <44182224+rajakadri@users.noreply.github.com>
2018-11-30 15:57:54 -0500

    Add files via upload
    Adding buttons and more jQuery functions

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ $(window).scroll(function(){
 let scroll = $(window).scrollTop();
 if (scroll > 300) {
 $("#visNavBar").fadeOut()
-$("#hiddenNav").fadeOut()}
+$("#hiddenNav").fadeOut()
+$("#dropdown").hide()}
 else {
 $("#hiddenNav").fadeIn()
 $("#visNavBar").fadeIn()}
+})
+
+$("#mainButton").click(function() {
+$("#firstButton").fadeToggle()
+})
+
+$("#firstButton").click(function() {
+$("#secondButton").fadeToggle()
+})
+
+$("#secondButton").click(function() {
+$("#thirdButton").fadeToggle()
 })
 });
