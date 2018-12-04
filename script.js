1b84d21d34dd3d7c59facedd8e2e9d803c7f0cf6
rajakadri <44182224+rajakadri@users.noreply.github.com>
2018-12-04 11:57:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-$(window).scroll(function(){
-let scroll = $(window).scrollTop();
-if (scroll > 300) {
-$("#visNavBar").fadeOut()
-$("#hiddenNav").fadeOut()
-$("#dropdown").hide()}
-else {
-$("#hiddenNav").fadeIn()
-$("#visNavBar").fadeIn()}
-})
 
 $("#mainButton").click(function() {
 $("#firstButton").fadeToggle()
@@ -21,4 +11,24 @@ $("#secondButton").fadeToggle()
 $("#secondButton").click(function() {
 $("#thirdButton").fadeToggle()
 })
+
+$(window).scroll(function(){
+let scroll = $(window).scrollTop();
+if (scroll > 300) {
+$("#visNavBar").fadeOut()
+$("#hiddenNav").fadeOut()
+$("#dropdown").hide()
+$("#scrollBtn").fadeIn()
+$("#scrollBtn").click(function(){
+$(window).scrollTop(0)
+})
+}
+else {
+$("#hiddenNav").fadeIn()
+$("#visNavBar").fadeIn()
+$("#dropdown").hide()
+$("#scrollBtn").fadeOut()}
+})
+
+
 });
